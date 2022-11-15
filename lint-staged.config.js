30af5713b29998cc6b18b7831ffd8a4bb8c52923
Arthur Ming <guobin.ming@pwc.com>
2022-11-15 16:10:29 +0800

    build: setup angular eslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,1 +1,10 @@
-module.exports = {}
+module.exports = {
+  "packages/dev-app/**/*.ts?(x)": (filenames) =>
+    filenames.length > 10
+      ? "yarn ng lint dev-app"
+      : `eslint --format stylish ${filenames.join(" ")}`,
+  "packages/ngx-dom-confetti/**/*.ts?(x)": (filenames) =>
+    filenames.length > 10
+      ? "yarn ng lint ngx-dom-confetti"
+      : `eslint --format stylish ${filenames.join(" ")}`,
+};
