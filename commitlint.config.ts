f041213c10b762efb05f3f7ccb7d6bf220a1e61e
Arthur Ming <guobin.ming@pwc.com>
2023-07-25 15:03:33 +0800

    chore: use commitlint.config.ts to instead of commitlint.config.js

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import type { UserConfig } from '@commitlint/types';
+
+const config: UserConfig = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "type-enum": [
@@ -18,9 +20,12 @@ module.exports = {
         "revert",
       ],
     ],
-    "subject-full-stop": [0, "never"],
-    "subject-case": [0, "never"],
-    "header-max-length": [2, "always", "200"],
-    "scope-case": [0, "never"],
+    "subject-full-stop": [0, 'never', '.'],
+    "subject-case": [0, "never", 'lower-case'],
+    "header-max-length": [2, "always", 200],
+    "scope-case": [2, "never", 'lower-case'],
   },
-};
+
+}
+
+module.exports = config;
