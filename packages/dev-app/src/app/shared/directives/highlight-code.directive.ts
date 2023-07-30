809c728b5e4ee6753e3eef47baa720d7b98471d5
Arthur Ming <guobin.ming@pwc.com>
2023-07-30 20:32:14 +0800

    chore: lazy load highlight.js

--- packages/dev-app/src/app/shared/directives/highlight-code.directive.ts
+++ packages/dev-app/src/app/shared/directives/highlight-code.directive.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Directive, ElementRef, SecurityContext } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import hljs from 'highlight.js';
+// import hljs from 'highlight.js';
 
 @Directive({
   selector: 'code[highlight]',
@@ -11,14 +11,17 @@ export class HighlightCodeDirective implements AfterViewInit {
   constructor(private el: ElementRef, private sanitizer: DomSanitizer) { }
 
   ngAfterViewInit() {
-    const text = (this.el.nativeElement as HTMLElement).innerHTML;
-    const sanitzed = this.sanitizer.sanitize(
-      SecurityContext.HTML,
-      text
-    );
-    if (sanitzed) {
-      hljs.highlightElement(this.el.nativeElement);
-    }
+    import('highlight.js').then(m => {
+      const text = (this.el.nativeElement as HTMLElement).innerHTML;
+      const sanitzed = this.sanitizer.sanitize(
+        SecurityContext.HTML,
+        text
+      );
+      if (sanitzed) {
+        m.default.highlightElement(this.el.nativeElement);
+      }
+    })
+
 
   }
 }
