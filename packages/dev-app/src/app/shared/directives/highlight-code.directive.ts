68a4561ffcf6f5857307feee935c8ecf0aeb6a29
Arthur Ming <guobin.ming@pwc.com>
2023-07-30 20:24:13 +0800

    style: lint fix

--- packages/dev-app/src/app/shared/directives/highlight-code.directive.ts
+++ packages/dev-app/src/app/shared/directives/highlight-code.directive.ts
@@ -1,23 +1,16 @@
-import { AfterContentInit, AfterViewInit, Directive, ElementRef, HostBinding, SecurityContext } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, SecurityContext } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import hljs from 'highlight.js';
 
 @Directive({
   selector: 'code[highlight]',
 })
-export class HighlightCodeDirective implements AfterViewInit, AfterContentInit {
+export class HighlightCodeDirective implements AfterViewInit {
 
-  // @HostBinding("innerHtml")
-  // content!: string
 
   constructor(private el: ElementRef, private sanitizer: DomSanitizer) { }
 
-  ngAfterContentInit(): void {
-    //console.log(this.content)
-  }
-
   ngAfterViewInit() {
-    // console.log(this.content)
     const text = (this.el.nativeElement as HTMLElement).innerHTML;
     const sanitzed = this.sanitizer.sanitize(
       SecurityContext.HTML,
