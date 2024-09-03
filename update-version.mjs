ed690b35be815421d52c62061502a4060e184b04
Arthur Ming <guobin.ming@pwc.com>
2024-09-03 22:28:33 +0800

    refactor: switch upldate-version to es module

---
+++ update-version.mjs
@@ -0,0 +1,31 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Read the main package.json file
+const mainPackageJsonPath = path.join(__dirname, "package.json");
+const mainPackageJson = JSON.parse(
+  fs.readFileSync(mainPackageJsonPath, "utf8"),
+);
+
+// Get the version from the main package.json
+const mainVersion = mainPackageJson.version;
+
+// Get all package.json files in the packages directory
+const packagesDir = path.join(__dirname, "packages");
+const packageJsonFiles = fs
+  .readdirSync(packagesDir)
+  .filter((file) => file.endsWith("package.json"))
+  .map((file) => path.join(packagesDir, file));
+
+// Update the version in each package.json
+packageJsonFiles.forEach((packageJsonPath) => {
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+  packageJson.version = mainVersion;
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+});
+
+console.log(`Version ${mainVersion} sync completed.`);
