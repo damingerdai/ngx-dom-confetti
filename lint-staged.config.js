bbc093c3cbcbb433581b8bf79ec45fab09e36202
damingerdai <mingguobin@live.com>
2026-05-22 11:05:24 +0800

    chore: migrate to npm

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   "packages/dev-app/**/*.ts?(x)": (filenames) =>
     filenames.length > 10
-      ? "yarn ng lint dev-app"
+      ? "npx ng lint dev-app"
       : `eslint --format stylish ${filenames.join(" ")}`,
   "packages/ngx-dom-confetti/**/*.ts?(x)": (filenames) =>
     filenames.length > 10
-      ? "yarn ng lint ngx-dom-confetti"
+      ? "npx ng lint ngx-dom-confetti"
       : `eslint --format stylish ${filenames.join(" ")}`,
 };
